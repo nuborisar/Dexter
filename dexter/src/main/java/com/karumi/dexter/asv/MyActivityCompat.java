e7eec3c21f758a0195d9a3b0cb3494f00e76227f
Alberto Sainz Vegas <nuborisar@gmail.com>
2016-07-19 16:17:31 +0200

    prechange

--- dexter/src/main/java/com/karumi/dexter/asv/MyActivityCompat.java
+++ dexter/src/main/java/com/karumi/dexter/asv/MyActivityCompat.java
@@ -1,9 +1,42 @@
 package com.karumi.dexter.asv;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotationox.NonNull;
+import android.support.v4ox.app.ActivityCompatApi23;
+
 /**
  * Created by nubor on 19/07/2016.
  */
 public class MyActivityCompat {
 
-    
+    public static void requestPermissions(final @NonNull Activity activity,
+                                          final @NonNull String[] permissions, final int requestCode) {
+        if (Build.VERSION.SDK_INT >= 23) {
+            ActivityCompatApi23.requestPermissions(activity, permissions, requestCode);
+        } else if (activity instanceof OnRequestPermissionsResultCallback) {
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    final int[] grantResults = new int[permissions.length];
+
+                    PackageManager packageManager = activity.getPackageManager();
+                    String packageName = activity.getPackageName();
+
+                    final int permissionCount = permissions.length;
+                    for (int i = 0; i < permissionCount; i++) {
+                        grantResults[i] = packageManager.checkPermission(
+                                permissions[i], packageName);
+                    }
+
+                    ((OnRequestPermissionsResultCallback) activity).onRequestPermissionsResult(
+                            requestCode, permissions, grantResults);
+                }
+            });
+        }
+    }
 }
